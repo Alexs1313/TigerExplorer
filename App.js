0683c07bc18e292f34fccc12f868900a7fdeb348
Alexs1313 <aleshaalesha1313@gmail.com>
2025-03-01 15:01:47 +0200

    commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        backgroundColor: 'radial-gradient(circle, #d97b6c , #c41522)',
+      }}>
+      <Text>flexsss</Text>
+    </View>
+  );
+};
+
+export default App;
